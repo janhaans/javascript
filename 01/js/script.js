0d73bf80686661bc6148795b154069196e770552
Jan Haans <jan.haans@gmail.com>
2018-03-02 00:21:24 +0100

    Added example 01

---
+++ 01/js/script.js
@@ -0,0 +1,55 @@
+document.getElementById('item1').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item1').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item2').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item2').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item3').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item3').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item4').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item4').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item5').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item5').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item6').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item6').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item7').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item7').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item8').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item8').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item9').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item9').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item10').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item10').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item11').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item11').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item12').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item12').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item13').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item13').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item14').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item14').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item15').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item15').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+document.getElementById('item16').addEventListener('mouseover', mouseOverChangeColor);
+document.getElementById('item16').addEventListener('mouseleave', mouseLeaveChangeColor);
+
+function mouseOverChangeColor() {
+    this.style.backgroundColor = "blue";
+}
+
+function mouseLeaveChangeColor() {
+    this.style.backgroundColor = "deepskyblue";
+}
